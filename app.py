7d25c37eabc2a450258858e37a67c9fcc5bac614
lvnhan <lvnhan.net@gmail.com>
2024-08-16 17:59:55 +0700

    change port

--- app.py
+++ app.py
@@ -52,5 +52,6 @@ app.layout = dbc.Container(
 )
 
 if __name__ == "__main__":
-    app.run_server(debug=True, use_reloader=False)
+    # app.run_server(debug=True, use_reloader=False)
+    app.run_server(host="0.0.0.0", port=8050, debug=True, use_reloader=False)
 
